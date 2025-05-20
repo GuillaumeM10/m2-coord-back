5d2b0153befc1b0a8c2d432c9af967dbb206665a
Thrive <guillaumemengelatte@hotmail.fr>
2025-05-20 14:32:52 +0200

    feat: minio docker / module / connection to nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MinioModule } from './minio/minio.module';
+import { ConfigModule } from '@nestjs/config';
+import { FilesModule } from './files/files.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MinioModule,
+    FilesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
